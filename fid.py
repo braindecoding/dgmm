8262244f80921b2b56779010838ebd8a6021396e
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-05 19:08:06 +0700

    berhasil itung fid

--- fid.py
+++ fid.py
@@ -6,8 +6,10 @@ Created on Fri Jan  5 14:47:17 2024
 """
 from pytorch_fid import fid_score
 
-def calculate_fid(real_images, generated_images):
-    fid = fid_score.calculate_fid_given_paths([real_images, generated_images], batch_size=50, device='cuda', dims=2048)
+def calculate_fid(real_images, generated_images, batch_size=50):
+    fid = fid_score.calculate_fid_given_paths([real_images, generated_images], batch_size=min(batch_size, len(real_images)), device='cpu', dims=2048)
     return fid
-fid_value = calculate_fid("stim","rec")
-print('FID:', fid_value)
+
+if __name__ == '__main__':
+    fid_value = calculate_fid("stim","rec")
+    print('FID:', fid_value)
