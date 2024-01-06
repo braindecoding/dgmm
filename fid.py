a0808f867395450338f8f74b25a0a948f3f3f33f
Rolly Maulana Awangga <awangga@gmail.com>
2024-01-07 05:58:32 +0700

    add newline

--- fid.py
+++ fid.py
@@ -18,9 +18,9 @@ if __name__ == '__main__':
     intermediate_dim=int(sys.argv[2])
     batch_size=int(sys.argv[3])
     maxiter=int(sys.argv[4])
-    experimentname=str(K)+","+str(intermediate_dim)+"," + str(batch_size)+"," + str(maxiter)+","
+    experimentname=str(K)+","+str(intermediate_dim)+"," + str(batch_size)+"," + str(maxiter)
     # Calculate FID - Frechet Inception Distance (FID)
     fid_value = calculate_fid("stim","rec")
     print('FID:', fid_value)
     with open("FID_Results.csv", "a") as myfile:
-        myfile.write(experimentname+","+str(fid_value))
+        myfile.write(experimentname+","+str(fid_value)+"\n")
